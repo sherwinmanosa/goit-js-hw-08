36154ae43174dc287968f44e15a4ac166ddc6c85
sherwinmanosa <sherwin.manosa@gmail.com>
2024-04-17 18:36:28 +0800

    task01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('ul.gallery');
+
+const photosMarkup = createGalleryItem(galleryItems);
+function createGalleryItem(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `
+        <a class = 'gallery__item' href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>    
+    `;
+    })
+    .join('');
+}
+galleryContainer.insertAdjacentHTML('beforeend', photosMarkup);
+
+const galleryHandler = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+galleryHandler.on('show.SimpleLightbox');
